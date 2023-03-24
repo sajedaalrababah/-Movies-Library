c5409508d28ac0e49157c377917c0be97acfd7e0
sajedaalrababah <sajedaalrababah@icloud.com>
2023-03-24 20:18:37 +0000

    sdfg

--- server.js
+++ server.js
@@ -1,18 +1,28 @@
 'use strict';
 const express = require("express");
+const cors = require("cors");
+const axios=require('axios');
 const movieData= require('./data.json')
 const app = express();
-const port =3000
+
+require('dotenv').config();
+app.use(cors());
+const api_key=process.env.api_key
+const PORT =process.env.PORT;
 
 app.get("/", handleFirstRoute);
 app.get("/favorite", handleFavoritePage);
+app.get("/trending", handleTrending);
+app.get("/search", handleSearch);
+app.get("/discover", handleDiscover);
+app.get("/changes", handleChanges);
 app.get('/example',errorHandler2)
 app.use("*", handleNtFoundError)
 
 
 function handleFirstRoute(req, res) {
     let result = [];
-    let newMovie = new Movies(movieData.title, movieData.poster_path, movieData.overview);
+    let newMovie = new MovieDa(movieData.title, movieData.poster_path, movieData.overview);
     result.push(newMovie);
     res.json(result);
 }
@@ -35,26 +45,95 @@ function errorHandler2(req,res){
         res.status(500).send("Sorry, something went wrong")
     });
 }
+function handleTrending(req, res) {
+    const url = `https://api.themoviedb.org/3/trending/all/week?api_key=${api_key}`;
+    axios.get(url)
+        .then(info => {
+            console.log(info.data.results);
+            let results = info.data.results.map(result => {
+                return new MovieDa(result.id, result.title, result.release_date, result.poster_path, result.overview);
+            })
+            res.json(results);
+        })
+        .catch(error => {
+            console.log(error);
+            res.send("inside error");
+        })
+}
+
+function handleSearch(req, res) {
+    let movieName = req.query.movieName;
+    const url = `https://api.themoviedb.org/3/search/company?api_key=${api_key}&page=1&query=${movieName}&api_key=${api_key}`
+    axios.get(url)
+        .then(info => {
+            res.json(info.data.results);
+        })
+        .catch(error => {
+            console.log(error);
+            res.send("inside error");
+        })
+}
+
+function handleDiscover(req, res) {
+    let movieName = req.query.movieName;
+    const url = `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate&query=${movieName}`
+
+    axios.get(url)
+        .then(info => {
+            res.json(info.data.results);
+        })
+        .catch(error => {
+            console.log(error);
+            res.send("inside error");
+        })
+}
 
 
 
-function Movies(title, poster_path, overview) {
-    this.title = title;
-    this.poster_path = poster_path;
-    this.overview = overview;
+function handleChanges(req, res) {
+    let movieName = req.query.movieName;
+    const url = `https://api.themoviedb.org/3/movie/changes?api_key=${api_key}&page=1query=${movieName}&api_key=${api_key}`
 
-
+    axios.get(url)
+        .then(info => {
+            res.json(info.data.results);
+        })
+        .catch(error => {
+            console.log(error);
+            res.send("inside error");
+        })
 }
 
 
 
 
+function MovieDa(id, title, release_date, poster_path, overview) {
+    this.id = id;
+    this.title = title;
+    this.release_date = release_date;
+    this.poster_path = poster_path;
+    this.overview = overview;
+
+}
+
+
+// function Movies(title, poster_path, overview) {
+//     this.title = title;
+//     this.poster_path = poster_path;
+//     this.overview = overview;
+
+
+// }
 
 
 
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}`)
   })
 
 
